f5b373e1ec7bc67fb607ce25e7f8cb7a5b91bfe5
brito79 <bshayamano2002@gmail.com>
2025-09-20 16:53:55 +0200

    Bundling the whole app

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,13 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// ES module equivalent for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 dotenv.config();
@@ -15,17 +22,30 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'http://localhost:5173'
-    }
-));
+
 app.use(rateLimiter);
 app.use((req, res, next) => {
     console.log(`The method is ${req.method} and the url is ${req.url}`);
     next();
 })
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+));
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist', 'index.html'));
+    });
+}
+
 
 
 connectDB().then(() => {
